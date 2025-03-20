615ea979bc9e54dc8d6bade3149862226238deb7
Trần Văn Khởi <tvkhoi123456@gmail.com>
2025-03-21 00:06:25 +0700

    Thay đổi vị trí của file MainActivity

--- app/app/src/main/java/com/example/musicapp/activities/MainActivity.java
+++ app/app/src/main/java/com/example/musicapp/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.musicapp;
+package com.example.musicapp.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.musicapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
